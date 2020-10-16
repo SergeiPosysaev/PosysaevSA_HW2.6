7e6a1aacc3d5ff99bb64a63059a1f08a3c26f734
Sergei Posysaev <fran@ustlab-1c.ru>
2020-10-16 13:55:38 +0300

    Final result

--- PosysaevSA_HW2.2.1/ViewController.swift
+++ PosysaevSA_HW2.2.1/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewForColors: UIView!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redValueSlider: UISlider!
+    @IBOutlet weak var greenValueSlider: UISlider!
+    @IBOutlet weak var blueValueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewForColors.backgroundColor = setNewColor()
+        
     }
 
-
+    private func setValuesLbl() {
+        redValueLabel.text = String(roundf(redValueSlider.value * 10) / 10)
+        greenValueLabel.text = String(roundf(greenValueSlider.value * 10) / 10)
+        blueValueLabel.text = String(roundf(blueValueSlider.value * 10) / 10)
+    }
+    private func setNewColor() -> UIColor {
+        UIColor(red: CGFloat(redValueSlider.value),
+                green: CGFloat(greenValueSlider.value),
+                blue: CGFloat(blueValueSlider.value),
+                alpha: 1)
+    }
+    
+    
+    
+    @IBAction func redActionSlider() {
+        setValuesLbl()
+        viewForColors.backgroundColor = setNewColor()
+       
+    }
+    
+    @IBAction func greenActionSlider() {
+        setValuesLbl()
+        viewForColors.backgroundColor = setNewColor()
+    }
+    
+    @IBAction func blueActionSlider() {
+        setValuesLbl()
+        viewForColors.backgroundColor = setNewColor()
+    }
+    
 }
+
+
 
